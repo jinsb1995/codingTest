255ce56e40dbdef0c05fbb69726b559af8211600
jinsb1995 <jinsb1995@gmail.com>
2022-03-10 19:31:08 +0900

    Marathon

---
+++ src/day1/Marathon_renew.java
@@ -0,0 +1,25 @@
+package day1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Maraton_renew {
+
+	public static void main(String[] args) {
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion  = {"eden", "kiki"};
+//		String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+//		String[] completion  = {"josipa", "filipa", "marina", "nikola"};
+//		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+//		String[] completion  = {"stanko", "ana", "mislav"};
+
+
+		
+
+
+
+	}
+	
+
+}
